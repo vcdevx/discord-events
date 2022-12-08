ae2b7a39bd6b279563bd67ac803d918c9e029c7c
Van <vcdevx@gmail.com>
2022-12-07 20:02:06 -0500

    Updated client settings for bot to go online. Testing commands

---
+++ src/commands/tools/ping.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require("discord.js");
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Return my ping!"),
+  async execute(interaction, client) {
+    const message = await interaction.deferReply({
+      fetchReply: true,
+    });
+
+    const newMessage = `API Latency: ${
+      client.ws.ping
+    }\nClient Ping: ${(message.createdTimestampd =
+      interaction.createdTimestampd)}`;
+    await interaction.editReply({
+      content: newMessage,
+    });
+  },
+};
